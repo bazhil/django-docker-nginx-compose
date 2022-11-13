e693f9b9827a21972078a7f74657d926e4db2a15
bazhil <i-bazh@mail.ru>
2022-11-13 21:46:18 +0300

    Finish new learn project, by tutorial: https://www.youtube.com/watch?v=nh1ynJGJuT8

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '$d5c5h&vkazayd$nevzv2%xrexgv9jv0rauzx(+ue^-0)##&ly'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'change_me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
